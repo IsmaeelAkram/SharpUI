d7d1500269f5cbea63bb96981fd08301e6c36043
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-06-10 12:11:07 -0400

    Make card component

---
+++ sharp/components/Card.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export default styled.TouchableOpacity`
+	background-color: #fff;
+	border-radius: 10px;
+	padding: 15px;
+	shadow-color: #000;
+	shadow-opacity: 0.1;
+	shadow-radius: 10px;
+`;
